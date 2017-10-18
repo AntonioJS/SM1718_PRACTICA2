779d58fb328b0d74904442c350e1233936bfff23
Donca <ajs00016@red.ujaen.es>
2017-10-18 12:25:53 +0200

    Comenzando práctica 2,  añadiendo parámetros y puerto por defecto.

--- app/src/main/java/es/ujaen/git/sm1718_practica2/MainActivity.java
+++ app/src/main/java/es/ujaen/git/sm1718_practica2/MainActivity.java
@@ -1,14 +1,24 @@
 package es.ujaen.git.sm1718_practica2;
 
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import java.net.ServerSocket;
 
+//Si está en gris es que no está siendo usado.
 
 public class MainActivity extends AppCompatActivity {
+    private ServerSocket serverSocket;
+
+    Handler UIHandler;
+    Thread Thread1 = null;
+
+    private EditText EDITTTEXT;
 
     private String datosvolatiles = "Hola";
     private TextView volatil = null;
@@ -24,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
 
         volatil = (TextView) findViewById(R.id.volatil);
         volatil.setText(datosvolatiles);
+ //Poniendo parámetros, aun no adaptado a nuestra practica.
+        EDITTTEXT = (EditText) findViewById(R.id.editText_login_user);
+        EDITTTEXT = (EditText) findViewById(R.id.editText_login_pass);
+        EDITTTEXT = (EditText) findViewById(R.id.editText_login_ip);
+        EDITTTEXT = (EditText) findViewById(R.id.editText_login_port);
 
     }
 
